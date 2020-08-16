b61463fa8ec67913107eba08ed194a2821ccec41
Dominick Brasileiro <brasileiro.dominick@gmail.com>
2020-08-16 04:39:03 -0300

    Add data and texts

---
+++ src/data/index.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    title: 'Focus on the work that matters',
+    description:
+      'Dropbox is the world’s first smart workspace. We bring all your team’s content together while letting you use the tools you love. And we help cut through the clutter, surfacing what matters most.',
+  },
+  {
+    title: 'Say goodbye to busywork',
+    description:
+      'Most “productivity” tools get in your way and constantly disrupt your flow. There’s a more enlightened way to work. Dropbox helps people be organized, stay focused, and get in sync with their teams.',
+  },
+  {
+    title: 'File storage and sharing are just the beginning',
+    description:
+      'Be organized, bring traditional files, cloud content, Dropbox Paper docs, and web shortcuts together in one place—and work the way that works for you.',
+  },
+  {
+    title: 'For everyone',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+  {
+    title: 'Bottom of the page',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis.',
+  },
+];
